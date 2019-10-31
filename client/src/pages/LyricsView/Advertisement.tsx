76c69ae80301db94019f42522d17f57b130ff926
Brent Vollebregt <brent.vollebregt@gmail.com>
2019-11-01 10:42:22 +1300

    Simplify ad checks and use the player to show an ad is playing

--- client/src/pages/LyricsView/Advertisement.tsx
+++ client/src/pages/LyricsView/Advertisement.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import PlayerStatus from './sharedComponents/PlayerStatus';
+import SpotifyLogoRound from '../../img/spotify-logo-round.png';
 
-const Advertisement: React.FunctionComponent = () => {
-    return <Container className="text-center">
-        <h2>Advertisement</h2>
-        <p>No song sorry</p>
+const adDuration = 30 * 1000; // Typically 30s
+
+interface IProps {
+    current: SpotifyApi.CurrentlyPlayingObject;
+    token?: string;
+}
+
+const Advertisement: React.FunctionComponent<IProps> = (props: IProps) => {
+    const { current, token } = props;
+    return <Container>
+        <PlayerStatus
+            albumArtUrl={SpotifyLogoRound}
+            title="Advertisement"
+            artist="Spotify"
+            album=""
+            durationMs={adDuration}
+            progressMs={current.progress_ms !== null ? current.progress_ms : 0}
+            isPlaying={current.is_playing}
+            spotifyToken={token}
+        />
     </Container>;
 };
 
