6fc23eccf358ab0d4a5516c42a4efbaed3b5bfcb
Brent Vollebregt <brent.vollebregt@gmail.com>
2019-11-02 04:56:09 +1300

    Use REACT_APP_API_ROOT as an override for using the current route

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,6 +1,7 @@
 export default {
     api: {
-        root: process.env.NODE_ENV === 'production' ? window.location.origin : process.env.REACT_APP_API_ROOT,
+        root: process.env.REACT_APP_API_ROOT ? process.env.REACT_APP_API_ROOT : window.location.origin,
+        // root: process.env.NODE_ENV === 'production' ? window.location.origin : process.env.REACT_APP_API_ROOT,
         spotify_authentication_route: '/api/spotify/authenticate'
     }
 };
