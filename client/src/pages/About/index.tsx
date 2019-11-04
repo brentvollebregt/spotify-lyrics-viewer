1d5d7fa5d1fd7d0e19d66806e7e8a12007ff4ba4
Brent Vollebregt <brent.vollebregt@gmail.com>
2019-11-04 16:25:12 +1300

    Current about

--- client/src/pages/About/index.tsx
+++ client/src/pages/About/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const About: React.FunctionComponent = () => {
-    return <Container className="text-center">
-        <h2>About</h2>
+    return <Container>
+        <Row className="justify-content-md-center">
+            <Col className="col-md-10 col-lg-8">
+                <h2 className="text-center">About</h2>
+                <p>Spotify Lyrics Viewer is a tool that allows you to view the lyrics of the current playing song on Spotify.</p>
+                <p>To do this, we first log you into Spotify so we can see the current song playing. The title and artist are then used to try and find the lyrics on <a href="https://genius.com/">GENIUS</a> and whatever lyrics matched the best are displayed to you.</p>
+            </Col>
+        </Row>
     </Container>;
 };
 
