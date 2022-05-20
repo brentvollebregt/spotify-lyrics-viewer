cd363385a0093a405ddf0c5d0f178d23ddf25b47
Brent Vollebregt <brent.vollebregt@gmail.com>
2022-05-21 00:46:36 +1200

    Add sitemap

---
+++ client/tools/copy-index.ts
@@ -0,0 +1,21 @@
+import fs from "fs";
+
+const rootDestinations = ["/", "/about"];
+
+// Create a 404 fallback for GitHub Pages
+fs.copyFile("build/index.html", `build/404.html`, err => {
+  if (err) throw err;
+  console.log(`Copied build/index.html to build/404.html`);
+});
+
+// Copy index.html to path folders to be served by GitHub Pages
+rootDestinations.forEach(dest => {
+  const fullDestination = `build/${dest}`;
+  if (!fs.existsSync(fullDestination)) {
+    fs.mkdirSync(fullDestination);
+  }
+  fs.copyFile("build/index.html", `${fullDestination}/index.html`, err => {
+    if (err) throw err;
+    console.log(`Copied build/index.html to ${fullDestination}/index.html`);
+  });
+});
---
+++ client/tools/generate-sitemap.ts
@@ -0,0 +1,14 @@
+import { SitemapStream, streamToPromise } from "sitemap";
+import { Readable } from "stream";
+import fs from "fs";
+
+const rootDestinations = ["/", "/about"];
+const siteUrl = "https://spotify-lyrics-viewer.nitratine.net";
+
+const links = rootDestinations.map(dest => ({ url: `${dest}`, priority: 0.8 }));
+
+const stream = new SitemapStream({ hostname: siteUrl });
+streamToPromise(Readable.from(links).pipe(stream)).then(data => {
+  fs.writeFileSync("./public/sitemap.xml", data);
+  console.log("Sitemap created.");
+});
